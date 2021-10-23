a9c4592bfa26b19b585efdf963c0e9e21ea3524b
raffaeleggdimaio <48028866+raffaeleggdimaio@users.noreply.github.com>
2021-10-23 12:05:47 +0200

    Prototipo parcheggio 2
    Aggiunta sbarra.

---
+++ SoftwareEngineeringProject/Codice/PrototipoParcheggio2/ParcheggioConcorrenteSbarra/src/progettoParcheggio/Launcher.java
@@ -0,0 +1,15 @@
+package progettoParcheggio;
+
+public class Launcher
+{
+	public static void main(String args[]) {
+		Parcheggio server = new GestoreParcheggio();
+		
+      	Thread entranteThread = new Thread(new AutoEntrante(server));
+      	Thread uscenteThread = new Thread(new AutoUscente(server));
+      	
+      	entranteThread.start();
+      	uscenteThread.start();
+	}
+}
+
