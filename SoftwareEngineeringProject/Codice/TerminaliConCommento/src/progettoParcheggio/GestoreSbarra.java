789f1b4f579bba7c2a55ceacccbf65ddec671cf2
raffaeleggdimaio <48028866+raffaeleggdimaio@users.noreply.github.com>
2021-11-06 13:36:02 +0100

    Aggiunta commenti nel codice.
    Commenti sui terminali e stream.

---
+++ SoftwareEngineeringProject/Codice/TerminaliConCommento/src/progettoParcheggio/GestoreSbarra.java
@@ -0,0 +1,12 @@
+package progettoParcheggio;
+
+public class GestoreSbarra
+{	
+	public void up() {
+		System.out.print("Innalzamento sbarra in corso.. \n");
+	}
+
+	public void down() {
+		System.out.print("Abbassamento sbarra in corso.. \n\n");
+	}
+}
